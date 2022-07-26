666435504885c0818fe7a4af651c40076b17d1a8
Therman116220 <thermanhaynes@gmail.com>
2022-07-26 11:12:30 -0500

    Update

--- src/main/java/com/portfolio/application/controller/RegistrationController.java
+++ src/main/java/com/portfolio/application/controller/RegistrationController.java
@@ -5,6 +5,7 @@ import com.portfolio.application.model.Registration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Map;
 
@@ -16,6 +17,13 @@ public class RegistrationController {
                                                /*URL:Reg , called model.Reg w/ an instance of reg*/
     public String getRegistration(@ModelAttribute ("registration")Registration registration) {
 
+        return "registration";
+    }
+
+    @PostMapping("registration")
+    /*URL:Reg , called model.Reg w/ an instance of reg*/
+    public String addRegistration(@ModelAttribute ("registration")Registration registration) {
+        System.out.println("Registration: " + registration.getName());
         return "registration";
     }
 }
