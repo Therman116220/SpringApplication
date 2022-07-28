25fcd7991d50cd92f00739185404f0fb93329c12
Therman116220 <thermanhaynes@gmail.com>
2022-07-28 00:01:35 -0500

    bug fix

--- src/main/java/com/portfolio/application/controller/GreetingController.java
+++ src/main/java/com/portfolio/application/controller/GreetingController.java
@@ -15,7 +15,7 @@ import java.util.Map;
 @Controller
 public class GreetingController {
 
-@GetMapping("greeting")
+@GetMapping("")
                        /* Map:String/Object Pair, w/ parameter name of "model" */
 public String greeting(Map<String, Object> model) {
     model.put("message", "Welcome Mike");
