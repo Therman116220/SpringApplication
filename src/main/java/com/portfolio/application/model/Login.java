d22389febd7d129c657cda09cfbd2084454add9a
Therman116220 <thermanhaynes@gmail.com>
2022-08-04 23:33:56 -0500

    update

--- src/main/java/com/portfolio/application/model/Login.java
+++ src/main/java/com/portfolio/application/model/Login.java
@@ -2,6 +2,15 @@ package com.portfolio.application.model;
 
 public class Login {
     private String Id;
+    private String Password;
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
 
     public String getId() {
         return Id;
